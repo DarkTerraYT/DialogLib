b2834f0b71620aee3d2a8ab342089d93d80de1b4
DarkTerraYT <liamwolfguy@gmail.com>
2025-05-12 22:13:14 -0400

    Fix RepoName

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -9,6 +9,6 @@ public static class ModHelperData
     public const string Description = "Library to make adding dialog to mods simple. Only add dialog to the queue after a match has started.";
 
     public const string RepoOwner = "DarkTerraYT"; // TODO add your github username hero, also in the download url in README.md
-    public const string RepoName = "DialogLib"; // TODO add your repo name here, also in the download url in README.md
+    public const string RepoName = "DialogueLib"; // TODO add your repo name here, also in the download url in README.md
     public const string Author = "LerpMcGerk"; // TODO add your repo name here, also in the download url in README.md
 }
